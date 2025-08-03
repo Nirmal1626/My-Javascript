696156f5c8aae8f0561108cfc13cf0d3ba05792a
Nirmal1626 <rkp16feb2006@gmail.com>
2025-08-03 03:36:54 +0000

    Rishabh's commit message

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+let accountEmail = "rkp16feb2006@gmail.com";
+var accountPassword = "12345";
+let accountCity = "Haridwar";
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com";
+accountPassword = "Rishabh@1";
+accountCity = "Greater Noida";
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
