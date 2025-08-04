baaf4a77d206bb9af05a602801cf2c682bd8f6ba
Nirmal1626 <rkp16feb2006@gmail.com>
2025-08-04 16:19:11 +0000

    My Introduction

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let name = "Rishabh";
+let age = 19;
+let isStudent  = true;
+let height = 5.10;
+let hobbies = ["Gaming", "Coding" , "Sports"];
+let address = {
+    city: "Haridwar",
+    state: "Uttrakhand",
+    country: "India"
+}
+let dateOfBirth = new Date("2006-02-16");
+let uniqueId = ("24SCSE1460028");
+console.log(name, age, isStudent, height, hobbies, address, dateOfBirth, uniqueId);
